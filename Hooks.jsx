bbc61836c868748bf47172ffc8d0148c6ba142b9
Prabhat Solanki <155136172+prabhats-github@users.noreply.github.com>
2024-02-16 15:39:16 +0530

    Add files via upload

--- Hooks.jsx
+++ Hooks.jsx
@@ -4,13 +4,16 @@ import st from "./Hooks.module.css"
 
 
 const Hooks = () => {
-    const [color,setColor]=useState("green")
+    const [color,setColor]=useState("green");
+ 
+    
+
   return (
     <div className={st.maindiv}>
         <h1> Color is {color}</h1>
-        <button onClick={()=>setColor("Red")}>Red Color</button>
-        
-        <button onClick={()=>setColor("Gray")}>Gray Color</button>
+        <button onClick={()=>setColor("Red") } className={st.btn1}>Red Color</button>
+
+        <button onClick={()=>setColor("Gray")} className={st.btn2}>Gray Color</button>
     </div>
   )
 }
